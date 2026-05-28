5eeb374defd6fb819c2c3182ac83d940320a0b86
rubensasa <rebensasa@gmail.com>
2026-05-28 21:06:04 +0200

    pre-codificado

--- src/app.js
+++ src/app.js
@@ -1,22 +1,31 @@
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function () {
+window.onload = function () 
+{
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-  let numberWho = Math.floor(Math.random() * who.length);
-  let numberAction = Math.floor(Math.random() * action.length);
-  let numberWhat = Math.floor(Math.random() * what.length);
-  let numberWhen = Math.floor(Math.random() * when.length);
   
+  function generarExcusa() {
 
+    let numberWho = Math.floor(Math.random() * who.length);
+    let numberAction = Math.floor(Math.random() * action.length);
+    let numberWhat = Math.floor(Math.random() * what.length);
+    let numberWhen = Math.floor(Math.random() * when.length);
 
-  document.getElementById("excuse").innerHTML = who[numberWho] + " " + action[numberAction] + " " + what[numberWhat] + " " + when[numberWhen];
+    document.querySelector("#excuse").innerHTML = who[numberWho] + " " + action[numberAction] + " " + what[numberWhat] + " " + when[numberWhen];
+  }
+  
+generarExcusa();
+
+  const boton = document.getElementById("pulsar");
+
+  boton.addEventListener("click", generarExcusa);
   console.log("Hello Rigo from the console!");
 };
